85fbb9b5719694f098b48c3843ca8ee2e4923dbd
David Cako <kingcako@gmail.com>
2017-02-06 15:58:11 -0700

    yee boi

---
+++ 03/a.rs
@@ -0,0 +1,32 @@
+use std::io::prelude::*;
+use std::fs::File;
+use std::io::BufReader;
+
+fn main() {
+    let f = File::open("input.txt").unwrap();
+    let reader = BufReader::new(f);
+    let mut possible_count: u32 = 0;
+
+    'lines: for line in reader.lines() {
+        let line: String = line.unwrap();
+        if !line.is_empty(){
+            let mut triangle: Vec<u32> = line.split_whitespace()
+                                         .map(|x| x.parse::<u32>().unwrap())
+                                         .collect();
+            'sides: for _ in 0..3 {
+                let side = triangle.remove(0);
+                println!("{} {}", triangle[0], triangle[1]);
+                if triangle[0] + triangle[1] > side {
+                    triangle.push(side);
+                    continue 'sides;
+                } else {
+                    continue 'lines;
+                }
+            }
+            possible_count += 1; 
+        }
+    }
+
+    println!("possible count: {}", possible_count);
+}
+        

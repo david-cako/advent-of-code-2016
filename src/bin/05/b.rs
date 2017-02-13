4abb7af9257878e6b0fedcf76be33f0e72f906c0
David Cako <kingcako@gmail.com>
2017-02-13 11:54:50 -0700

    5.b

---
+++ src/bin/05/b.rs
@@ -0,0 +1,42 @@
+extern crate crypto;
+
+use crypto::md5::Md5;
+use crypto::digest::Digest;
+
+const INPUT: &'static str = "ojvtpuvg";
+
+fn check_hash(input: &str, md5: &mut Md5) -> Option<(usize, char)> {
+        let mut result = String::from("");
+        md5.input_str(input);
+        result = md5.result_str();
+        md5.reset();
+
+        let mut characters = result.chars();
+        if result.starts_with("00000") {
+            let pos: u8 = characters.nth(5).unwrap().to_string().parse().unwrap();
+            return Some((pos as usize, characters.next().unwrap()));
+        } else {
+            return None;
+        }
+}
+
+
+fn main() {
+    let mut md5 = Md5::new();
+    let mut iter: u64 = 0;
+    let mut password: [char; 8] = ['0', '0', '0', '0', '0', '0', '0', '0'];
+    let mut inserted: [bool; 8] = [false, false, false, false, false, false, false, false];
+
+    loop {
+        let input = INPUT.to_string() + iter.to_string().as_str();
+        if let Some((pos, character)) = check_hash(input.as_str(), &mut md5) {
+            password[pos] = character;
+            inserted[pos] = true;
+        }
+        iter += 1;
+        if inserted.iter().filter(|&&x| x == true).count() == 8 {
+            println!("password: {}", password.iter().cloned().collect::<String>());
+            break;
+        }
+    }
+}

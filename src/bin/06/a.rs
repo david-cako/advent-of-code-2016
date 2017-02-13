4147179891c3af3e39f656a0f109f12f1f61f36f
David Cako <dc@cako.io>
2017-02-13 16:29:14 -0700

    start on 6

---
+++ src/bin/06/a.rs
@@ -0,0 +1,38 @@
+use std::io::prelude::*;
+use std::fs::File;
+use std::io::BufReader;
+use std::collections::HashMap;
+
+fn main() {
+    let f = File::open("input.txt").unwrap();
+    let reader = BufReader::new(f);
+    let char_map: [Vec<char>; 8] = [Vec::new(), Vec::new(), Vec::new(), Vec::new(), 
+                                    Vec::new(), Vec::new(), Vec::new(), Vec::new()];
+    let message: Vec<char> = Vec::new();
+
+    for line in reader.lines() {
+        let line: String = line.unwrap();
+        if !line.is_empty() {
+            for let Some((i, character)) in line.chars().enumerate() {
+                char_map[i].push(character);
+            }
+        }
+    }
+    for let Some((i, char_vec)) in char_map.iter().enumerate() {
+        let char_counter = HashMap::new();
+        for character in char_vec.iter() {
+            let counter = char_counter.entry(character).or_insert(0);
+            *counter += 1;
+        }
+        let mut largest: (char, u16) = ('', 0);
+        for (character, val) in char_counter.iter() {
+            if val > largest[1] {
+                largest = (character, val);
+            }
+        }
+        message.push(largest[0]);
+    }
+
+    println!("message: {}", message);
+}
+

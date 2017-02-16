2a10c37e9dbd1201862b8a004337fb242f7f9b0e
David Cako <dc@cako.io>
2017-02-16 13:22:38 -0700

    add day creation script

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Year of the Rust systems programming language (TM) edition
 
 soul level 1, std library only.
 
+day creation script creates boilerplate files and adds them to Cargo.toml:
+
+    cargo run --bin create_day
+
+must be invoked from numbered day directory such as `advent-of-code-2016/src/bin/02`.
+
--- src/bin/06/a.rs
+++ src/bin/06/a.rs
@@ -6,33 +6,33 @@ use std::collections::HashMap;
 fn main() {
     let f = File::open("input.txt").unwrap();
     let reader = BufReader::new(f);
-    let char_map: [Vec<char>; 8] = [Vec::new(), Vec::new(), Vec::new(), Vec::new(), 
+    let mut char_map: [Vec<char>; 8] = [Vec::new(), Vec::new(), Vec::new(), Vec::new(), 
                                     Vec::new(), Vec::new(), Vec::new(), Vec::new()];
-    let message: Vec<char> = Vec::new();
+    let mut message: Vec<char> = Vec::new();
 
     for line in reader.lines() {
         let line: String = line.unwrap();
         if !line.is_empty() {
-            for let Some((i, character)) in line.chars().enumerate() {
+            for (i, character) in line.chars().enumerate() {
                 char_map[i].push(character);
             }
         }
     }
-    for let Some((i, char_vec)) in char_map.iter().enumerate() {
-        let char_counter = HashMap::new();
+    for char_vec in char_map.iter() {
+        let mut char_counter = HashMap::new();
         for character in char_vec.iter() {
             let counter = char_counter.entry(character).or_insert(0);
             *counter += 1;
         }
-        let mut largest: (char, u16) = ('', 0);
+        let mut largest: (char, u16) = ('0', 0);
         for (character, val) in char_counter.iter() {
-            if val > largest[1] {
-                largest = (character, val);
+            if *val > largest.1 {
+                largest = (**character, *val);
             }
         }
-        message.push(largest[0]);
+        message.push(largest.0);
     }
-
+    let message: String = message.into_iter().collect();
     println!("message: {}", message);
 }
 

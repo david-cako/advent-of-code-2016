4c81c5ccb74cd2e96cd207ff7121b6cc5e936263
David Cako <dc@cako.io>
2017-03-21 10:57:50 -0600

    need to figure out how to make my clone logic work...

--- src/bin/11/a.rs
+++ src/bin/11/a.rs
@@ -1,23 +1,20 @@
 use std::io::prelude::*;
 use std::fs::File;
 use std::io::BufReader;
+use std::collections::VecDeque;
 
-fn is_vaid(elevator: Vec<&str>, dest: Vec<&str>) -> bool {
+#[derive(Clone)]
+struct State {
+    floors: [Vec<String>; 4],
+    moves: u16,
+}
+
+fn is_vaid(floor: Vec<String>) -> bool {
     let mut is_valid = true;
-    
-    if elevator.contains("LM") {
-        if dest.contains("HG") && !dest.contains("LG") {
-            is_valid = false;
-        }
-        if elevator.contains("HG") {
-            is_valid = false;
-        }
-    }
-    if elevator.contains("HM") {
-        if dest.contains("LG") && !dest.contains("HG") {
-            is_valid = false;
-        }
-        if elevator.contains("LG") {
+
+    for each in floor.clone().iter() {
+        let (element, object) = each.split_at(1);
+        if object == "M" && !floor.contains(element + "G") {
             is_valid = false;
         }
     }
@@ -27,9 +24,58 @@ fn is_vaid(elevator: Vec<&str>, dest: Vec<&str>) -> bool {
 
 
 fn main() {
-    for floor in floors.iter.cycle() {
+    let queue: VecDeque<State> = VecDeque::new();
+    queue.push_back(
+        State { floors: [vec!["SG", "SM", "PG", "PM"],
+                         vec!["TG", "RG", "RM", "CG", "CM"],
+                         vec!["TM"],
+                         vec![]], moves: 0 }
+    );
+    'outer: while queue.len() > 0 {
         // priority: move 2 up, move 1 up, else keep one and move down?
-
-    println!("message: {}", message);
+        let state = queue.pop_back().unwrap();
+        let state_iter = state.iter();
+        for (floor_i, floor) in state_iter.enumerate() {
+            for each in floor.clone().iter() {
+                for other_each in floor.iter() {
+                    if each != other_each {
+                        let mut next_floor = state.floors[floor_i + 1].clone();
+                        next_floor.push(each).push(other_each);
+                        if is_valid(next_floor) {
+                            let mut new_state = state.clone();
+                            new_state.floors[floor_i].remove(each);
+                            new_state.floors[floor_i].remove(other_each);
+                            new_state.floors[floor_i + 1].push(each);
+                            new_state.floors[floor_i + 1].push(other_each);
+                            new_state.moves += 1;
+                            queue.push_front(new_state);
+                            println!("floor {}: {:?}, moves: {}", floor_i, next_floor, new_state.moves); 
+                            continue 'outer;
+                        }
+                    }
+                }
+                let mut next_floor = state.floors[floor_i + 1].clone();
+                next_floor.push(each);
+                if is_valid(next_floor) {
+                    let mut new_state = state.clone();
+                    new_state.floors[floor_i].remove(each);
+                    new_state.floors[floor_i + 1] = next_floor;
+                    new_state.moves += 1;
+                    queue.push_front(new_state);
+                    println!("floor {}: {:?}, moves: {}", floor_i, next_floor, new_state.moves); 
+                }
+                let mut previous_floor = state.floors[floor_i - 1].clone();
+                previous_floor.push(each);
+                if is_valid(previous_floor) {
+                    let mut new_state = state.clone();
+                    new_state.floors[floor_i].remove(each);
+                    new_state.floors[floor_i - 1] = previous_floor;
+                    new_state.moves += 1;
+                    queue.push_front(new_state);
+                    println!("floor {}: {:?}, moves: {}", floor_i, previous_floor, new_state.moves); 
+                }
+            }
+        }
+    }
 }
 
